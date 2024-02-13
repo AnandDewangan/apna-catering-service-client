f2439713c2f339ff442f2b19f9506ed3c9f4d918
Anand Dewangan <118416821+AnandDewangan@users.noreply.github.com>
2024-02-13 11:02:32 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -65,62 +65,7 @@ export default function Team() {
                     </div>
                 </div>
             </div>
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                <div className="team-item text-center rounded overflow-hidden">
-                    <div className="rounded-circle overflow-hidden m-4">
-                        <img className="img-fluid" src={require("../images/team-2.jpg")} alt="" />
-                    </div>
-                    <h5 className="mb-0">Full Name</h5>
-                    <small>Designation</small>
-                    <div className="d-flex justify-content-center mt-3">
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                <div className="team-item text-center rounded overflow-hidden">
-                    <div className="rounded-circle overflow-hidden m-4">
-                        <img className="img-fluid" src={require("../images/team-3.jpg")} alt="" />
-                    </div>
-                    <h5 className="mb-0">Full Name</h5>
-                    <small>Designation</small>
-                    <div className="d-flex justify-content-center mt-3">
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                <div className="team-item text-center rounded overflow-hidden">
-                    <div className="rounded-circle overflow-hidden m-4">
-                        <img className="img-fluid" src={require("../images/team-4.jpg")} alt="" />
-                    </div>
-                    <h5 className="mb-0">Full Name</h5>
-                    <small>Designation</small>
-                    <div className="d-flex justify-content-center mt-3">
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-                <div className="team-item text-center rounded overflow-hidden">
-                    <div className="rounded-circle overflow-hidden m-4">
-                        <img className="img-fluid" src={require("../images/team-1.jpg")} alt="" />
-                    </div>
-                    <h5 className="mb-0">Full Name</h5>
-                    <small>Designation</small>
-                    <div className="d-flex justify-content-center mt-3">
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-twitter"></i></a>
-                        <a className="btn btn-square btn-primary mx-1" href=""><i className="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-            </div>
+      
         </div>
     </div>
 </div>
